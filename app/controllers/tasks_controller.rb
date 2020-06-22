4e9607e7b895e6bbe62f128c01587b6558805e53
kanta.yamada <yamada.kanta@lmi.ne.jp>
2020-06-22 10:26:21 +0000

    post

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,63 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  
+  def index
+    @tasks = current_user.tasks
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  
+  
+  private
+  
+  def set_task
+    @task = current_user.tasks.find_by(id:params[:id])
+    unless @task
+      redirect_to'/'
+    end
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+
+end
