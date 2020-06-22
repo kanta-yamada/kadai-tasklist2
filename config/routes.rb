0dbc301792bba65d6e90a21ec0d472f54fb1295c
kanta.yamada <yamada.kanta@lmi.ne.jp>
2020-06-22 12:25:54 +0000

    routes and before_action

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destory'
-  get 'users/new'
-  get 'users/create'
+  #get 'sessions/new'
+  #get 'sessions/create'
+  #get 'sessions/destory'
+  #get 'users/new'
+  #get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "tasks#index"
   
